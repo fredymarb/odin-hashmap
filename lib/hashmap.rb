f2c760e877fa09f3fc5c584d8013b38b3a3b4dde
fredymarb <29468132+fredymarb@users.noreply.github.com>
2024-08-21 04:03:17 +0000

    feat: #set method assigns a key value pair to the HashMap

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,3 +1,5 @@
+require_relative "node"
+
 class HashMap
   INITIAL_SIZE = 16
   LOAD_FACTOR = 0.75
@@ -14,6 +16,30 @@ class HashMap
 
     key.each_char { |char| hash_code = (prime_number * hash_code) + char.ord }
 
-    hash_code % @buckets
+    hash_code % @buckets.length
+  end
+
+  # assigns key value pair to the HashMap
+  def set(key, value)
+    index = hash(key)
+    raise IndexError if index.negative? || index >= @buckets.length
+
+    new_node = Node.new(key, value)
+    return @buckets[index] = new_node if @buckets[index].nil?
+
+    current = @buckets[index]
+    until current.next.nil?
+      # if key already exist, update it's value
+      return current.value = value if current.key == key
+
+      current = current.next
+    end
+
+    current.next = new_node
   end
 end
+
+my_hash = HashMap.new
+my_hash.set("name", "Fred")
+my_hash.set("age", "21")
+my_hash.set("region", "Accra")
